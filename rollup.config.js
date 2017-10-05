4ef13c40a639f3d76971b974cbe011a2d32599c8
Mike Kovarik <kenr.mk@gmail.com>
2017-10-05 18:50:44 +0200

    correct UMD global import names

--- rollup.config.js
+++ rollup.config.js
@@ -4,14 +4,22 @@ import fs from 'fs'
 var pkg = fs.readFileSync('package.json')
 pkg = JSON.parse(pkg.toString())
 
-// TODO: define globals with list of Node core modules to prevent bundle to import 'events'
-//       as window.$1events
+var nodeCoreModules = require('repl')._builtinLibs
+var globals = objectFromArray(nodeCoreModules)
 
 export default {
 	input: 'index.mjs',
 	output: {
+		sourcemap: true,
 		file: `index.js`,
 		format: 'umd',
 	},
 	name: pkg.name,
+	globals,
+}
+
+function objectFromArray(arr) {
+	var obj = {}
+	arr.forEach(moduleName => obj[moduleName] = moduleName)
+	return obj
 }
